b445f9e39e6791f5c790381c19b39ddecf2da043
Nasser Alsaqabi <alsokor4@gmail.com>
2020-12-27 13:09:35 +0300

    Created Calculator

---
+++ SwiftUI-hw1/SwiftUI-hw1/ContentView.swift
@@ -0,0 +1,162 @@
+//
+//  ContentView.swift
+//  SwiftUI-hw1
+//
+//  Created by NasserAlsaqabi on 27/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack(alignment:.bottom){
+            Color.black.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack(spacing: 12){
+                HStack{
+                    Spacer()
+                    Text("555")
+                        .foregroundColor(.white)
+                        .font(.system(size:64))
+                }
+                .padding()
+                    HStack{
+                        Text("C")
+                            .frame(width: 80, height: 80)
+                            .foregroundColor(.white)
+                            .background(Color.gray)
+                            .cornerRadius(40)
+                        
+                        Text("±")
+                            .frame(width: 80, height: 80)
+                            .foregroundColor(.white)
+                            .background(Color.gray)
+                            .cornerRadius(40)
+                        Text("%")
+                            .frame(width: 80, height: 80)
+                            .foregroundColor(.white)
+                            .background(Color.gray)
+                            .cornerRadius(40)
+                        Text("/")
+                            .frame(width: 80, height: 80)
+                            .foregroundColor(.white)
+                            .background(Color.yellow)
+                            .cornerRadius(40)
+                    }
+                    .font(.system(size: 32))
+                    
+                HStack{
+                    Text("7")
+                        .frame(width: 80, height: 80)
+                        .foregroundColor(.white)
+                        .background(Color.gray)
+                        .cornerRadius(40)
+                    
+
+                    Text("8")
+                        .frame(width: 80, height: 80)
+                        .foregroundColor(.white)
+                        .background(Color.gray)
+                        .cornerRadius(40)
+                    Text("9")
+                        .frame(width: 80, height: 80)
+                        .foregroundColor(.white)
+                        .background(Color.gray)
+                        .cornerRadius(40)
+                    Text("x")
+                        .frame(width: 80, height: 80)
+                        .foregroundColor(.white)
+                        .background(Color.yellow)
+                        .cornerRadius(40)
+                }
+                .font(.system(size: 32))
+                
+                
+                HStack{
+                    Text("4")
+                        .frame(width: 80, height: 80)
+                        .foregroundColor(.white)
+                        .background(Color.gray)
+                        .cornerRadius(40)
+                    Text("5")
+                        .frame(width: 80, height: 80)
+                        .foregroundColor(.white)
+                        .background(Color.gray)
+                        .cornerRadius(40)
+                    Text("6")
+                        .frame(width: 80, height: 80)
+                        .foregroundColor(.white)
+                        .background(Color.gray)
+                        .cornerRadius(40)
+                    Text("-")
+                        .frame(width: 80, height: 80).foregroundColor(.white)
+                        .background(Color.yellow)
+                        .cornerRadius(40)
+                    
+                }
+                .font(.system(size: 32))
+
+                HStack{
+                    Text("1")
+                        .frame(width: 80, height: 80)
+                        .foregroundColor(.white)
+                        .background(Color.gray)
+                        .cornerRadius(40)
+                    Text("2")
+                        .frame(width: 80, height: 80)
+                        .foregroundColor(.white)
+                        .background(Color.gray)
+                        .cornerRadius(40)
+                    Text("3")
+                        .frame(width: 80, height: 80)
+                        .foregroundColor(.white)
+                        .background(Color.gray)
+                        .cornerRadius(40)
+                    Text("+")
+                        .frame(width: 80, height: 80)
+                        .foregroundColor(.white)
+                        .background(Color.yellow)
+                        .cornerRadius(40)
+                }
+                .font(.system(size: 32))
+
+                HStack{
+                    Text("0")
+                        .padding()
+                        .frame(width: 80, height: 80)
+                        .foregroundColor(.white)
+                        .background(Color.gray)
+                        .cornerRadius(40)
+                    
+                    Text(".")
+                        .frame(width: 80, height: 80)
+                        .foregroundColor(.white)
+                        .background(Color.gray)
+                        .cornerRadius(40)
+ 
+                    Text(".")
+                        .frame(width: 80, height: 80)
+                        .foregroundColor(.white)
+                        .background(Color.gray)
+                        .cornerRadius(40)
+                    Text("=")
+                        .frame(width: 80, height: 80)
+                        .foregroundColor(.white)
+                        .background(Color.yellow)
+                        .cornerRadius(40)
+                }
+                .font(.system(size: 32))
+
+            }
+            
+
+        }
+        //.background(Color.black)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice("iPhone 11 Pro")
+    }
+}
---
+++ SwiftUI-hw1/SwiftUI-hw1/SwiftUI_hw1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_hw1App.swift
+//  SwiftUI-hw1
+//
+//  Created by NasserAlsaqabi on 27/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_hw1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
